804e63628cf5ac8c5daf570f910f02a0ce2c05e0
Yuval <yuval@coti.io>
2021-05-19 21:18:46 +0300

    added data service and implemented crappy service

--- crappy.service.ts
+++ crappy.service.ts
@@ -1,14 +1,38 @@
+import {accounts} from './data.service'
 export class CrappyService {
-    withdrawal(id: number, amount: number): Promise<{oldAmount: number, newAmount: number}> {
-      throw new Error('not implemented yet');
+    async withdrawal(id: number, amount: number): Promise<{oldBalance: number, newBalance: number}> {
+        const oldBalance = accounts[id];
+        const random = Math.random();
+
+        if (random > 0.75) {
+            throw new Error('random error');
+        }
+        const randomSleepSeconds: number = (Math.floor(random * 5) + 3) * 1000;
+        await new Promise(r => setTimeout(r, randomSleepSeconds));
+        accounts[id] = oldBalance - amount;
+        return {oldBalance, newBalance: accounts[id]}
     }
-    deposit(id: number, amount: number): Promise<{oldAmount: number, newAmount: number}> {
-        throw new Error('not implemented yet');
+    async deposit(id: number, amount: number): Promise<{oldBalance: number, newBalance: number}> {
+        const oldBalance = accounts[id];
+        const random = Math.random();
+
+        if (random < 0.25) {
+            throw new Error('random error');
+        }
+        const randomSleepSeconds: number = (Math.floor(random * 5) + 3) * 1000;
+        await new Promise(r => setTimeout(r, randomSleepSeconds));
+        accounts[id] = oldBalance + amount;
+        return {oldBalance, newBalance: accounts[id]}
       }
-    deleteBalance(id: number): Promise<void> {
-    throw new Error('not implemented yet');
+    async deleteBalance(id: number): Promise<void> {
+        delete accounts[id];
+        return;
     }
-    getBalance(id: number): Promise<number> {
-    throw new Error('not implemented yet');
+    async getBalance(id: number): Promise<number> {
+        if (!accounts[id]) {
+            throw new Error('account does not exist');
+        }
+        return accounts[id];
+
     }
   }
